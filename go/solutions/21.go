2a1b183f5108b2d8a2b421ac8d36929b2ace723a
abdullahnettoor <abdullahnettoor@gmail.com>
2024-05-08 21:51:02 +0530

    Added Solution(Go) Q:21

--- go/solutions/21.go
+++ go/solutions/21.go
@@ -59,3 +59,19 @@ func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
 	}
 	return dummy.Next
 }
+
+// * Solution 2 -- Using recursion
+// func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
+// 	if l1 == nil {
+// 		return l2
+// 	}
+// 	if l2 == nil {
+// 		return l1
+// 	}
+// 	if l1.Val < l2.Val {
+// 		l1.Next = mergeTwoLists(l1.Next, l2)
+// 		return l1
+// 	}
+// 	l2.Next = mergeTwoLists(l1, l2.Next)
+// 	return l2
+// }
