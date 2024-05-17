10919914fa3c4934231eef1a469bcd6a605556c2
abdullahnettoor <abdullahnettoor@gmail.com>
2024-05-17 09:12:29 +0530

    Done Solution(Go) Q:108

---
+++ go/solutions/108.go
@@ -0,0 +1,49 @@
+package solutions
+
+/* Question **********************************
+? 108. Convert Sorted Array to Binary Search Tree
+Given an integer array nums where the elements are sorted in ascending order, convert it to a
+height-balanced binary search tree.
+
+Example 1:
+Input: nums = [-10,-3,0,5,9]
+Output: [0,-3,9,-10,null,5]
+Explanation: [0,-10,5,null,-3,null,9] is also accepted:
+
+Example 2:
+Input: nums = [1,3]
+Output: [3,1]
+Explanation: [1,null,3] and [3,1] are both height-balanced BSTs.
+
+
+Constraints:
+1 <= nums.length <= 104
+-104 <= nums[i] <= 104
+nums is sorted in a strictly increasing order.
+*********************************************/
+
+func Output108() any {
+	return sortedArrayToBST([]int{5, 10, 11, 14, 25, 30})
+}
+
+// type TreeNode struct {
+// 	Val   int
+// 	Left  *TreeNode
+// 	Right *TreeNode
+// }
+
+// * Solution 1 -- Recursion -- Using helper function -- Time & Space O(n)
+func sortedArrayToBST(nums []int) *TreeNode {
+	return sortedArrayToBSTHelper(0, len(nums)-1, nums)
+}
+
+func sortedArrayToBSTHelper(start, end int, nums []int) *TreeNode {
+	if start > end {
+		return nil
+	}
+	mid := (start + end) / 2
+	node := &TreeNode{Val: nums[mid]}
+	node.Left = sortedArrayToBSTHelper(start, mid-1, nums)
+	node.Right = sortedArrayToBSTHelper(mid+1, end, nums)
+	return node
+}
