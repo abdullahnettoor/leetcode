c6782b8aa8cb75b61474a51754525c7b9e7e2ef5
abdullahnettoor <abdullahnettoor@gmail.com>
2024-05-26 21:54:26 +0530

    Added Solution(Go) Q:136

--- go/solutions/136.go
+++ go/solutions/136.go
@@ -68,4 +68,21 @@ func singleNumber(nums []int) int {
 //     return nums[ptr1]
 // }
 
+// * Solution -- Brute Force -- Using Nested Loops -- Time O(n^2) - Space O(1)
+// func singleNumber(nums []int) int {
+// 	for i := 0; i < len(nums); i++ {
+// 		count := 0
+// 		for j := 0; j < len(nums); j++ {
+// 			if nums[i] == nums[j] {
+// 				count++
+// 			}
+// 		}
+// 		if count == 1 {
+// 			return nums[i]
+// 		}
+// 	}
+// 	return -1
+// }
+
+
 
