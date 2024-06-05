6eb71cc8a72c28e8c1a285a63298641740f74245
abdullahnettoor <abdullahnettoor@gmail.com>
2024-06-05 22:22:47 +0530

    Done Solution(Go) Q:122

---
+++ go/solutions/122.go
@@ -0,0 +1,45 @@
+package solutions
+
+/* Question *******************************
+? 121. Best Time to Buy and Sell Stock
+You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+
+Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+
+Example 1:
+Input: prices = [7,1,5,3,6,4]
+Output: 5
+Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+
+Example 2:
+Input: prices = [7,6,4,3,1]
+Output: 0
+Explanation: In this case, no transactions are done and the max profit = 0.
+
+Constraints:
+1 <= prices.length <= 10^5
+0 <= prices[i] <= 10^4
+******************************************/
+
+func Output122() any {
+	return maxProfit122([]int{7, 1, 5, 3, 6, 4})
+}
+
+// * Solution -- Using Two Pointers -- Greedy -- Time O(n) - Space O(1)
+func maxProfit122(prices []int) int {
+    l, r := 0, 1 
+    profit := 0
+    for r < len(prices) {
+        if prices[l] < prices[r] {
+            profit += prices[r] - prices[l]
+            l = r
+        } else {
+            l++
+        }
+        r++
+    }
+    return profit
+}
