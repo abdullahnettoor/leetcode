6bfc048fbf26955e23cb4c9f16bbd814743c4c69
abdullahnettoor <abdullahnettoor@gmail.com>
2024-06-07 10:50:45 +0530

    Added Solution(Go) Q:49

--- go/solutions/49.go
+++ go/solutions/49.go
@@ -1,5 +1,7 @@
 package solutions
 
+import "sort"
+
 /* Question ********************************
 ? 49. Group Anagrams
 Given an array of strings strs, group the anagrams together. You can return the answer in any order.
@@ -28,26 +30,44 @@ func Output49() any {
 	return groupAnagrams([]string{"eat", "tea", "tan", "ate", "nat", "bat"})
 }
 
-// * Solution - Brute Force - Time O(n^2 × m) - Space O(n)
+// * Solution - Brute Force - Time O(n×mlogm) - Space O(mlogm)
 func groupAnagrams(strs []string) [][]string {
-	if len(strs) == 1 {
-		return [][]string{strs}
+	anagramMap := make(map[string][]string)
+	for _, w := range strs {
+		chars := []byte(w)
+		sort.Slice(chars, func(i, j int) bool {
+			return chars[i] < chars[j]
+		})
+		key := string(chars)
+		anagramMap[key] = append(anagramMap[key], w)
 	}
 	res := make([][]string, 0)
-	addedWords := make(map[string]bool)
-	for i := range strs {
-		if addedWords[strs[i]] {
-			continue
-		}
-		arr := []string{strs[i]}
-		addedWords[strs[i]] = true
-		for j := i + 1; j < len(strs); j++ {
-			if isAnagram(strs[i], strs[j]) {
-				arr = append(arr, strs[j])
-				addedWords[strs[j]] = true
-			}
-		}
-		res = append(res, arr)
+	for _, m := range anagramMap {
+		res = append(res, m)
 	}
 	return res
 }
+
+// * Solution - Brute Force - Time O(n^2 × m) - Space O(n)
+// func groupAnagrams(strs []string) [][]string {
+// 	if len(strs) == 1 {
+// 		return [][]string{strs}
+// 	}
+// 	res := make([][]string, 0)
+// 	addedWords := make(map[string]bool)
+// 	for i := range strs {
+// 		if addedWords[strs[i]] {
+// 			continue
+// 		}
+// 		arr := []string{strs[i]}
+// 		addedWords[strs[i]] = true
+// 		for j := i + 1; j < len(strs); j++ {
+// 			if isAnagram(strs[i], strs[j]) {
+// 				arr = append(arr, strs[j])
+// 				addedWords[strs[j]] = true
+// 			}
+// 		}
+// 		res = append(res, arr)
+// 	}
+// 	return res
+// }
