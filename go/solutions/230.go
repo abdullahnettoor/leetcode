45db81e816a6e1b5d02f360636360ed97c091a30
abdullahnettoor <abdullahnettoor@gmail.com>
2024-06-23 11:42:08 +0530

    Added Solution(Go) Q:230

--- go/solutions/230.go
+++ go/solutions/230.go
@@ -1,22 +1,23 @@
 package solutions
 
 /* Question **********************************
-? 230. Reverse String
-Write a function that reverses a string. The input string is given as an array of characters s.
-
-You must do this by modifying the input array in-place with O(1) extra memory.
+? 230. Kth Smallest Element in a BST
+Given the root of a binary search tree, and an integer k, return the kth smallest value (1-indexed) of all the values of the nodes in the tree.
 
 Example 1:
-Input: s = ["h","e","l","l","o"]
-Output: ["o","l","l","e","h"]
+Input: root = [3,1,4,null,2], k = 1
+Output: 1
 
 Example 2:
-Input: s = ["H","a","n","n","a","h"]
-Output: ["h","a","n","n","a","H"]
+Input: root = [5,3,6,2,4,null,null,1], k = 3
+Output: 3
 
 Constraints:
-1 <= s.length <= 105
-s[i] is a printable ascii character.
+The number of nodes in the tree is n.
+1 <= k <= n <= 104
+0 <= Node.val <= 104
+
+Follow up: If the BST is modified often (i.e., we can do insert and delete operations) and you need to find the kth smallest frequently, how would you optimize?
 *********************************************/
 
 func Output230() any {
@@ -35,7 +36,7 @@ func Output230() any {
  * }
  */
 
-// * Solution -- DFS - Recursion -- Time O(n) - Space O(1)
+// * Solution -- DFS - Recursion -- Time O(H+k) - Space O(1)
 func kthSmallest(root *TreeNode, k int) int {
 	var res, ctr int
 	var inOrder func(node *TreeNode)
@@ -71,8 +72,29 @@ func kthSmallest(root *TreeNode, k int) int {
 // 		arr = inOrder(node.Right, arr)
 // 		return arr
 // 	}
-
 // 	arr := inOrder(root, []int{})
-
 // 	return arr[k-1]
+// }
+
+// * Solution -- DFS - Iteration -- Time O(H) - Space O(H)
+// * Worst Case will be O(n) for Time & Space for skewed trees
+// func kthSmallest(root *TreeNode, k int) int {
+// 	stack := []*TreeNode{}
+// 	curr := root
+// 	for curr != nil || len(stack) > 0 {
+// 		if curr != nil {
+// 			stack = append(stack, curr)
+// 			curr = curr.Left
+// 		} else {
+// 			node := stack[len(stack)-1]
+// 			stack = stack[:len(stack)-1]
+// 			k--
+// 			if k == 0 {
+// 				curr = node
+// 				break
+// 			}
+// 			curr = node.Right
+// 		}
+// 	}
+// 	return curr.Val
 // }
