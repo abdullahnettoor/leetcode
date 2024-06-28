2854f76a2b4af882972d8f2050f0f712151a7f5b
abdullahnettoor <abdullahnettoor@gmail.com>
2024-06-28 16:09:00 +0530

    Done Solution(Go) Q:3264

---
+++ go/solutions/3264.go
@@ -0,0 +1,61 @@
+package solutions
+
+import "math"
+
+/* Question **********************************
+? 3264. Final Array State After K Multiplication Operations I
+You are given an integer array nums, an integer k, and an integer multiplier.
+
+You need to perform k operations on nums. In each operation:
+- Find the minimum value x in nums. If there are multiple occurrences of the minimum value, select the one that appears first.
+- Replace the selected minimum value x with x * multiplier.
+- Return an integer array denoting the final state of nums after performing all k operations.
+
+Example 1:
+Input: nums = [2,1,3,5,6], k = 5, multiplier = 2
+Output: [8,4,6,5,6]
+
+Explanation:
+Operation			Result
+After operation 1	[2, 2, 3, 5, 6]
+After operation 2	[4, 2, 3, 5, 6]
+After operation 3	[4, 4, 3, 5, 6]
+After operation 4	[4, 4, 6, 5, 6]
+After operation 5	[8, 4, 6, 5, 6]
+
+Example 2:
+Input: nums = [1,2], k = 3, multiplier = 4
+Output: [16,8]
+
+Explanation:
+Operation			Result
+After operation 1	[4, 2]
+After operation 2	[4, 8]
+After operation 3	[16, 8]
+
+
+Constraints:
+1 <= nums.length <= 100
+1 <= nums[i] <= 100
+1 <= k <= 10
+1 <= multiplier <= 5
+*********************************************/
+
+func Output3264() any {
+	return getFinalState([]int{2, 1, 3, 5, 6}, 5, 2)
+}
+
+// * Solution -- Brute force -- Time O(k*n) - Space O(1)
+func getFinalState(nums []int, k int, multiplier int) []int {
+	for k > 0 {
+		minIdx, minVal := 0, math.MaxInt
+		for idx, num := range nums {
+			if minVal > num {
+				minIdx, minVal = idx, num
+			}
+		}
+		nums[minIdx] *= multiplier
+		k--
+	}
+	return nums
+}
